e5a79c2f0e4b7ef17c7c8526d32e6942e1bc3c93
Josh <chaozhang78@icloud.com>
2015-08-25 14:48:18 -0400

    Added missing number problem

---
+++ LeetcodeInCSharp/Medium/BitManipulation/MissingNumber/Solution1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeInCSharp.Medium.BitManipulation.MissingNumber
+{
+    public class Solution1
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Length; i++)
+                res ^= (i + 1) ^ nums[i];
+            return res;
+        }
+    }
+}
---
+++ SolutionUnitTests/Medium/BitManipulation/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolutionUnitTests.Medium.BitManipulation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LeetcodeInCSharp.Medium.BitManipulation.MissingNumber.Solution1 test =
+                new LeetcodeInCSharp.Medium.BitManipulation.MissingNumber.Solution1();
+            Assert.AreEqual(test.MissingNumber(new int[] { 0 }), 1);
+        }
+    }
+}
