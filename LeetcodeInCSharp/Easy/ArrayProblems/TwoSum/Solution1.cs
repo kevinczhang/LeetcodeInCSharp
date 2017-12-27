804b21caf1ed0a8a58c2a9d9902b2853101ece32
Josh <chaozhang78@icloud.com>
2017-12-26 22:55:46 -0500

    Added two sum Solution

---
+++ LeetcodeInCSharp/Easy/ArrayProblems/TwoSum/Solution1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeInCSharp.Easy.ArrayProblems.TwoSum
+{
+    public class Solution1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] res = new int[2];
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    res[0] = map[nums[i]];
+                    res[1] = i;
+                    return res;
+                }
+                if(!map.ContainsKey(target - nums[i]))
+                    map.Add(target - nums[i], i);
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetcodeInCSharpTests/Easy/ArrayProblems/TwoSum/Solution1Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetcodeInCSharp.Easy.ArrayProblems.TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeInCSharp.Easy.ArrayProblems.TwoSum.Tests
+{
+    [TestClass()]
+    public class Solution1Tests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            Solution1 test = new Solution1();
+            int[] nums = { 2, 7, 11, 15 };
+            int[] res = test.TwoSum(nums, 9);
+            Assert.AreEqual(2, nums[0]);
+            Assert.AreEqual(7, nums[1]);
+        }
+    }
+}
